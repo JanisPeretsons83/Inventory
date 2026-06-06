395b8df0831f07dc2117cf8273a997ec35783328
JanisPeretsons83 <144663195+JanisPeretsons83@users.noreply.github.com>
2026-06-06 23:09:29 +0300

    Add files via upload

---
+++ lib/data/grade_data.dart
@@ -0,0 +1,14 @@
+class GradeData {
+  static const List<Map<String, String>> grades = [
+    {"code": "K", "desc": "Kvalitātes šķira"},
+    {"code": "A", "desc": "Augstākā šķira"},
+    {"code": "1a", "desc": "Ēvelēti dēļi"},
+    {"code": "1b", "desc": "Neēvelēti dēļi"},
+    {"code": "1c", "desc": "Sagataves"},
+    {"code": "1d", "desc": "Tālākā apstrādē"},
+    {"code": "2a", "desc": "Bez pielietojuma"},
+    {"code": "2b", "desc": "Brāķis izmantojams"},
+    {"code": "2c", "desc": "Brāķis iznīcināms"},
+    {"code": "PAL", "desc": "Paletes"},
+  ];
+}
